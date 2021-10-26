f1a3270afc191b9575b21fefefc86aee5379ad66
Christopher Strøm <detoxte@gmail.com>
2021-10-27 01:10:38 +0200

    Add first decent tuning parameters

--- Graded/G1/eskf/tuning_sim.py
+++ Graded/G1/eskf/tuning_sim.py
@@ -4,16 +4,16 @@ from datatypes.eskf_params import ESKFTuningParams
 from datatypes.eskf_states import NominalState, ErrorStateGauss
 
 tuning_params_sim = ESKFTuningParams(
-    accm_std=1.,
-    accm_bias_std=1.,
-    accm_bias_p=1.,
+    accm_std=0.01,
+    accm_bias_std=0.0005,
+    accm_bias_p=10e-12,
 
-    gyro_std=1.,
-    gyro_bias_std=1.,
-    gyro_bias_p=1.,
+    gyro_std=0.0002,
+    gyro_bias_std=0.0002,
+    gyro_bias_p=10e-12,
 
-    gnss_std_ne=1.,
-    gnss_std_d=1.)
+    gnss_std_ne=0.25,
+    gnss_std_d=0.5)
 
 x_nom_init_sim = NominalState(
     np.array([0., 0., 0.]),  # position
@@ -24,9 +24,9 @@ x_nom_init_sim = NominalState(
     ts=0.)
 
 init_std_sim = np.repeat(repeats=3,  # repeat each element 3 times
-                         a=[1.,  # position
-                            1.,  # velocity
-                            np.deg2rad(1),  # angle vector
-                            1.,  # accelerometer bias
-                            1.])  # gyro bias
+                         a=[10,  # position
+                            10,  # velocity
+                            np.deg2rad(np.pi/30),  # angle vector
+                            0.001,  # accelerometer bias
+                            0.001])  # gyro bias
 x_err_init_sim = ErrorStateGauss(np.zeros(15), np.diag(init_std_sim**2), 0.)
